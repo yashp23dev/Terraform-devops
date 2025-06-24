d3b8ca231faf7a9a6ca4eafd9fbb023401a21037
yash <yashppatilk23@gmail.com>
2025-06-24 15:40:50 +0000

    writing providers in separate file

--- providers/instance/createinstance.tf
+++ providers/instance/createinstance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-	access_key = "access_key"
-	secret_key = "secret_key"
-	region = "us-east-2"
-}
-
 resource "aws_instance" "MyFirstIstance" {
 	ami  = "ami-05692172625678b4e"
 	instance_type = "t2.micro"
