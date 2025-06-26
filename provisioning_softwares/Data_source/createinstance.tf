dbce4625b533e48ba3632f3cf8107f38a99d150b
yash <yashppatilk23@gmail.com>
2025-06-26 12:58:07 +0000

    datasource stream added

--- provisioning_softwares/Data_source/createinstance.tf
+++ provisioning_softwares/Data_source/createinstance.tf
@@ -1,7 +1,10 @@
+data "aws_availability_zones" "available" {}
+
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
     Name = "custom_instance"
