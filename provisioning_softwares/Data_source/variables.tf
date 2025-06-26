0434169e9df72e0736cb8cd79bb4a39f5996da8a
yash <yashppatilk23@gmail.com>
2025-06-26 12:40:08 +0000

    datasource demo

--- provisioning_softwares/Data_source/variables.tf
+++ provisioning_softwares/Data_source/variables.tf
@@ -23,14 +23,3 @@ variable "AMIS" {
     }
 }
 
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "levelup_key"
-}
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "levelup_key.pub"
-}
-
-variable "INSTANCE_USERNAME" {
-  default = "ubuntu"
-}
