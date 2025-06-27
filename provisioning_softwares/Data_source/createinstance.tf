bf69826834fb1126c24cfc7894b1ce31f1ea3dac
yash <yashppatilk23@gmail.com>
2025-06-27 11:08:27 +0000

    using daatasourc for getting the ami dynamically

--- provisioning_softwares/Data_source/createinstance.tf
+++ provisioning_softwares/Data_source/createinstance.tf
@@ -1,13 +1,30 @@
-data "aws_availability_zones" "available" {}
+
+data "aws_availability_zones" "avilable" {}
+
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
+  ami           = data.aws_ami.latest-ubuntu.id
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = data.aws_availability_zones.avilable.names[1]
+
 
   tags = {
     Name = "custom_instance"
   }
-
 }
+
