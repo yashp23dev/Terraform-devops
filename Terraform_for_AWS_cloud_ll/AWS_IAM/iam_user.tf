650db69f94a10550500fb880fddc8ee731e25e25
yash <yashppatilk23@gmail.com>
2025-07-03 15:59:35 +0000

    creating iam users and groups using Terraform

---
+++ Terraform_for_AWS_cloud_ll/AWS_IAM/iam_user.tf
@@ -0,0 +1,33 @@
+#TF file for IAM users and groups
+
+resource "aws_iam_user" "user1" {
+    name = "user1"    
+}
+
+resource "aws_iam_user" "user2" {
+    name = "user2"
+}
+
+# Group for IAM users
+
+resource "aws_iam_group" "group1" {
+    name = "group1"
+}
+
+# Attach IAM users to the group
+
+resource "aws_iam_user_group_membership" "admin-users" {
+    user = [
+        aws_iam_user.user1.name,
+        aws_iam_user.user2.name,
+    ]
+    groups = [aws_iam_group.group1.name]
+}
+
+# IAM policy for the group
+
+resource "aws_iam_policy_attachment" "group1-policy" {
+    name       = "group1-policy"
+    groups     = [aws_iam_group.group1.name]
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
