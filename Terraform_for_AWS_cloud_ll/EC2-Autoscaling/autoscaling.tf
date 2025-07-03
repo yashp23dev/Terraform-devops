7d5d0770d3ef5d9d53706c94518d27881ab5aa2d
yash <yashppatilk23@gmail.com>
2025-07-03 17:15:10 +0000

    added ssh key-pair in autoscaling instances

--- Terraform_for_AWS_cloud_ll/EC2-Autoscaling/autoscaling.tf
+++ Terraform_for_AWS_cloud_ll/EC2-Autoscaling/autoscaling.tf
@@ -8,6 +8,11 @@ resource "aws_launch_configuration" "Launch-config" {
 
 }
 
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
 # Autoscaling Group
 
 resource "aws_autoscaling_group" "AutoScalingGroup" {
