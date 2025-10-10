ade99ef324e627812b2df64d49a7035b4a2bad0d
yash <yashppatilk23@gmail.com>
2025-10-10 05:57:42 +0000

    github source added

--- Terraform_modules/main.tf
+++ Terraform_modules/main.tf
@@ -1,5 +1,5 @@
 module "ec2_cluster" {
-    source = ""
+    source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
     name = "my-ec2-cluster"
     ami  = "ami-0f40c8f97004632f9"  # Replace with a valid AMI ID
