dd55b320e7b41de2636a46273f2454ba982507de
yash <yashppatilk23@gmail.com>
2025-10-12 13:19:07 +0000

    vriables updated

--- Terraform_modules/NetworkModule/variable.tf
+++ Terraform_modules/NetworkModule/variable.tf
@@ -9,12 +9,12 @@ variable "environment_tag" {
 
 variable "public_key_path" {
   description = "Path to the public key"
-  default     = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/custom_key.pub"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-0c55b159cbfafe1f0" # use latest ami id hor the region 
+  default     = "ami-0c55b159cbfafe1f0" # use latest ami id for the region
 }
 
 variable "instance_type" {
